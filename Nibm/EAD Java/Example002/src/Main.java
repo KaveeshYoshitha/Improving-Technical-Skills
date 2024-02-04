44d1b7f86d241f7a1643abe4a71cef1dd8b74249
Kaveesh Yoshitha <yoshithakaveesh83@gmail.com>
2024-02-04 23:38:30 +0530

    Example 002 using Java Added

---
+++ Nibm/EAD Java/Example002/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class circleDemo {
+    private static Scanner sc = new Scanner(System.in);
+
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter the radius: ");
+
+        double radius = sc.nextDouble();
+        double area = Math.PI * (radius * radius);
+        System.out.println("The area of circle is: " + area);
+
+        double circumference = Math.PI * 2 * radius;
+        System.out.println("The circumference of the circle is:" + circumference);
+
+    }
+}
